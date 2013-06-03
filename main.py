0459df3606adffaec12c06f4e3e2e96b94db46a8
Eric Pelz <eric+github@ericpelz.com>
2013-06-03 15:37:50 -0700

    New auth view decorator + list of friends
    - New view decorator to verify that a user is logged in, and that they still
      have a valid facebook authentication token.
    - Basic template scaffolding in place for testing
    - Info page now shows a list of friends with their IDs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, redirect, url_for, session, request
+from functools import wraps
+from flask import Flask, redirect, url_for, session, request, render_template
 from flask_oauth import OAuth
 import os
 
@@ -23,7 +24,6 @@ facebook = oauth.remote_app('facebook',
     consumer_secret=FACEBOOK_SECRET,
     request_token_params={'scope': 'email'}
 )
-
 
 @app.route('/')
 def index():
@@ -56,7 +56,26 @@ def get_facebook_oauth_token():
     """ Return the access token. """
     return session.get('facebook_token')
 
+def inspect_facebook_token():
+    """ Checks that a facebook auth token is still valid. """
+    if 'facebook_token' not in session:
+        return False
+
+    token = session['facebook_token'][0]
+    inspect = facebook.get('/debug_token?input_token=' + token)
+
+    return inspect.status == 200 and inspect.data['data']['is_valid']
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not inspect_facebook_token():
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
 @app.route("/logout")
+@login_required
 def logout():
     """ Log out the user. """
     session.pop('logged_in', None)
@@ -64,12 +83,16 @@ def logout():
     return redirect(url_for('index'))
 
 @app.route('/info')
+@login_required
 def show_info():
     """ Print info about the facebook user. """
     me = facebook.get('/me')
     friends = facebook.get('/me/friends')
-    return 'Logged in as id=%s name=%s redirect=%s' % \
-       (me.data['id'], me.data['name'], request.args.get('next'))
+
+    friends = map(lambda x: (x['name'], x['id']), friends.data['data'])
+    return render_template('info.html', me=me.data, friends=friends)
+#    return 'Logged in as id=%s name=%s redirect=%s token=%s' % \
+#       (me.data['id'], me.data['name'], request.args.get('next'), session['facebook_token'])
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
